c3e74fc474895697c617f47aafa213348e742b58
Simone Mauro <simon.mauro@virgilio.it>
2022-08-02 16:46:59 +0200

    database completato

---
+++ Database/BoolflixContext.cs
@@ -0,0 +1,18 @@
+﻿using csharp_boolflix.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_boolflix.Database{
+    public class BoolflixContext : DbContext{
+
+        public DbSet<ContenutoVideo> Videos {get; set;}
+        public DbSet<Film> Films {get; set;}
+        public DbSet<Genere> Genres {get; set;}
+        public DbSet<PlayList> Playlists {get; set;}
+        public DbSet<Profilo> Users {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder){
+            builder.UseSqlServer("Data Source=localhost;Initial Catalog=boolflix;Integrated Security=True");
+        }
+
+    }
+}
---
+++ Migrations/20220802144506_InitialCreate.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace csharp_boolflix.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsBambino = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Videos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titolo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Data = table.Column<int>(type: "int", nullable: false),
+                    Novità = table.Column<bool>(type: "bit", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Videos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Playlists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titolo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfiloId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Playlists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Playlists_Users_ProfiloId",
+                        column: x => x.ProfiloId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContenutoVideoGenere",
+                columns: table => new
+                {
+                    ContenutiId = table.Column<int>(type: "int", nullable: false),
+                    GeneriId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContenutoVideoGenere", x => new { x.ContenutiId, x.GeneriId });
+                    table.ForeignKey(
+                        name: "FK_ContenutoVideoGenere_Genres_GeneriId",
+                        column: x => x.GeneriId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ContenutoVideoGenere_Videos_ContenutiId",
+                        column: x => x.ContenutiId,
+                        principalTable: "Videos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContenutoVideoPlayList",
+                columns: table => new
+                {
+                    ContenutiId = table.Column<int>(type: "int", nullable: false),
+                    PlayListsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContenutoVideoPlayList", x => new { x.ContenutiId, x.PlayListsId });
+                    table.ForeignKey(
+                        name: "FK_ContenutoVideoPlayList_Playlists_PlayListsId",
+                        column: x => x.PlayListsId,
+                        principalTable: "Playlists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ContenutoVideoPlayList_Videos_ContenutiId",
+                        column: x => x.ContenutiId,
+                        principalTable: "Videos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContenutoVideoGenere_GeneriId",
+                table: "ContenutoVideoGenere",
+                column: "GeneriId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContenutoVideoPlayList_PlayListsId",
+                table: "ContenutoVideoPlayList",
+                column: "PlayListsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Playlists_ProfiloId",
+                table: "Playlists",
+                column: "ProfiloId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContenutoVideoGenere");
+
+            migrationBuilder.DropTable(
+                name: "ContenutoVideoPlayList");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "Playlists");
+
+            migrationBuilder.DropTable(
+                name: "Videos");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
